57433c51ff54a2b9ebe140146537c9b16c94d2f3
Iti maheshwari <imaheshwari270@gmail.com>
2025-10-08 13:48:32 +0530

    This is the commit for web application

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+regmodel = pickle.load(open('regmodel.pkl', 'rb'))
+scalar = pickle.load(open('scaling.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
+    output = regmodel.predict(new_data)
+    return jsonify(output[0])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1, -1))
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text=f"The House price prediction is {output}")
+
+if __name__ == "__main__":
+    app.run(debug=True)
